2865d8c425926d4f47bc2087f908bd593d8502a2
starnas1 <104042208+starnas1@users.noreply.github.com>
2022-09-14 11:23:58 -0700

    Initial features created

--- README.md
+++ README.md
@@ -10,17 +10,16 @@ A mobile flashcard app to help you study wherever you are.
 
 <img src="YOUR_GIF_URL_HERE" width=200><br>
 
-`TODO://` In the User stories section below, add an `x` between the `-[ ]` like this `- [x]` for any user story you complete. (☝️ Remove this paragraph after after checking off user stories)
 
 ## Required
 - [x] Create New Project in Android Studio
-- [ ] Add a view for the front side of the flashcard to display the question
-- [ ] Add a view for the back side of the flashcard to display the answer
-- [ ] Build in logic to show the answer side when the card is tapped
-- [ ] Push code to GitHub
+- [x] Add a view for the front side of the flashcard to display the question
+- [x] Add a view for the back side of the flashcard to display the answer
+- [x] Build in logic to show the answer side when the card is tapped
+- [x] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
-   - [ ] Change the background color of the multiple choice answers when clicked to indicate whether the question was answered correctly
-- [ ] Further customize and style the card
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
+   - [x] Change the background color of the multiple choice answers when clicked to indicate whether the question was answered correctly
+- [x] Further customize and style the card
--- app/src/main/java/com/example/cramcards/MainActivity.kt
+++ app/src/main/java/com/example/cramcards/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.cramcards
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val multipleChoice2 = findViewById<TextView>(R.id.answer2)
+        val multipleChoice1 = findViewById<TextView>(R.id.answer1)
+        val multipleChoice3 = findViewById<TextView>(R.id.answer3)
+        val hideIcon = findViewById<ImageView>(R.id.toggle_choices_visibility)
+        var isShowingAnswers = true
+
+
+        hideIcon.setOnClickListener{
+            if (isShowingAnswers){
+                multipleChoice1.visibility = View.INVISIBLE
+                multipleChoice2.visibility = View.INVISIBLE
+                multipleChoice3.visibility = View.INVISIBLE
+                findViewById<ImageView>(R.id.toggle_choices_visibility).setImageResource(R.drawable.show_icon)
+                isShowingAnswers = false}
+            else {
+                multipleChoice1.visibility = View.VISIBLE
+                multipleChoice2.visibility = View.VISIBLE
+                multipleChoice3.visibility = View.VISIBLE
+                findViewById<ImageView>(R.id.toggle_choices_visibility).setImageResource(R.drawable.hide_icon)
+                isShowingAnswers = true}
+        }
+
+        flashcardQuestion.setOnClickListener {
+            it.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            it.visibility = View.INVISIBLE
+        }
+        multipleChoice2.setOnClickListener{
+        it.setBackgroundColor(resources.getColor(R.color.green_right))
+        }
+        multipleChoice1.setOnClickListener{
+            it.setBackgroundColor(resources.getColor(R.color.red_wrong))
+            multipleChoice1.setTextColor(resources.getColor(R.color.beige))
+            multipleChoice2.setBackgroundColor(resources.getColor(R.color.green_right))
+        }
+        multipleChoice3.setOnClickListener{
+            it.setBackgroundColor(resources.getColor(R.color.red_wrong))
+            multipleChoice3.setTextColor(resources.getColor(R.color.beige))
+            multipleChoice2.setBackgroundColor(resources.getColor(R.color.green_right))
+        }
     }
 }
